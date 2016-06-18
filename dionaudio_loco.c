603a4f447e58a7a48c03cc978b1a14c8b045f7d7
Miquel <miquelblauw@hotmail.com>
2016-06-18 20:18:05 +0200

    Update dionaudio_loco.c

--- dionaudio_loco.c
+++ dionaudio_loco.c
@@ -4,6 +4,8 @@
  * Author:      Miquel Blauw <info@dionaudio.nl>
  *              Copyright 2016
  *
+ * Based on the software of the RPi-DAC writen by Florian Meier  
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
