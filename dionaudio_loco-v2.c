e51c375823880daf7ab31e7e51b89532dcc8d245
Miquel <miquelblauw@hotmail.com>
2016-11-10 11:26:56 +0100

    Update dionaudio_loco-v2.c

--- dionaudio_loco-v2.c
+++ dionaudio_loco-v2.c
@@ -1,7 +1,8 @@
 /*
  * ASoC Driver for Dion Audio LOCO-V2 DAC-AMP
  *
- * Author:      Miquel Blauw <info@dionaudio.nl> *              Copyright 2016
+ * Author:      Miquel Blauw <info@dionaudio.nl>
+ *              Copyright 2016
  *
  * Based on the software of the RPi-DAC writen by Florian Meier
  *
@@ -11,39 +12,41 @@
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU * General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  */
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
 
-#include <sound/core.h>#include <sound/pcm.h>
+#include <sound/core.h>
+#include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
 #include <sound/jack.h>
+
 static bool digital_gain_0db_limit = true;
 
 static int snd_rpi_dionaudio_loco-v2_init(struct snd_soc_pcm_runtime *rtd)
 {
-	if (digital_gain_0db_limit)
-	{
+	if (digital_gain_0db_limit) {
 		int ret;
 		struct snd_soc_card *card = rtd->card;
 		struct snd_soc_codec *codec = rtd->codec;
 
-		ret = snd_soc_limit_volume(codec, "Digital Playback Volume", 207);		if (ret < 0)
+		ret = snd_soc_limit_volume(codec, "Digital Playback Volume", 207);
+		if (ret < 0)
 			dev_warn(card->dev, "Failed to set volume limit: %d\n", ret);
 	}
 
 	return 0;
 }
 
-static int snd_rpi_dionaudio_loco-v2_hw_params(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params)
+static int snd_rpi_dionaudio_loco-v2_hw_params(
+	struct snd_pcm_substream *substream,
+	struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-// NOT USED	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-// NOT USED	struct snd_soc_codec *codec = rtd->codec;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 
 	unsigned int sample_bits =
@@ -52,7 +55,8 @@ static int snd_rpi_dionaudio_loco-v2_hw_params(struct snd_pcm_substream *substre
 	return snd_soc_dai_set_bclk_ratio(cpu_dai, sample_bits * 2);
 }
 
-/* machine stream operations */static struct snd_soc_ops snd_rpi_dionaudio_loco-v2_ops = {
+/* machine stream operations */
+static struct snd_soc_ops snd_rpi_dionaudio_loco-v2_ops = {
 	.hw_params = snd_rpi_dionaudio_loco-v2_hw_params,
 };
 
@@ -85,26 +89,27 @@ static int snd_rpi_dionaudio_loco-v2_probe(struct platform_device *pdev)
 	snd_rpi_dionaudio_loco-v2.dev = &pdev->dev;
 
 	if (pdev->dev.of_node) {
-	    struct device_node *i2s_node;
-	    struct snd_soc_dai_link *dai = &snd_rpi_dionaudio_loco-v2_dai[0];
-	    i2s_node = of_parse_phandle(pdev->dev.of_node,
-					"i2s-controller", 0);
+		struct device_node *i2s_node;
+		struct snd_soc_dai_link *dai =
+					&snd_rpi_dionaudio_loco-v2_dai[0];
 
-	    if (i2s_node) {
-		dai->cpu_dai_name = NULL;
-		dai->cpu_of_node = i2s_node;
-		dai->platform_name = NULL;
-		dai->platform_of_node = i2s_node;
-	    }
+		i2s_node = of_parse_phandle(pdev->dev.of_node,
+					    "i2s-controller", 0);
+		if (i2s_node) {
+			dai->cpu_dai_name = NULL;
+			dai->cpu_of_node = i2s_node;
+			dai->platform_name = NULL;
+			dai->platform_of_node = i2s_node;
+		}
 
-		digital_gain_0db_limit = !of_property_read_bool(pdev->dev.of_node,
-					"dionaudio,24db_digital_gain");
+		digital_gain_0db_limit = !of_property_read_bool(
+			pdev->dev.of_node, "dionaudio,24db_digital_gain");
 	}
 
 	ret = snd_soc_register_card(&snd_rpi_dionaudio_loco-v2);
 	if (ret)
-		dev_err(&pdev->dev,
-			"snd_soc_register_card() failed: %d\n", ret);
+		dev_err(&pdev->dev, "snd_soc_register_card() failed: %d\n",
+			ret);
 
 	return ret;
 }
